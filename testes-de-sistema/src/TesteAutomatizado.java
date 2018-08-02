0e55aa34698f8668f22ebe6ae411750e8b2158f9
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-02 00:59:11 -0300

    Curso Selenium: Sua webapp testada de ponta a ponta em java, Aula 1

---
+++ testes-de-sistema/src/TesteAutomatizado.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteAutomatizado {
+
+    public static void main(String[] args) {
+    	
+    	//tutorial para configurar o geckodriver
+    	//http://taketest.take.net/2016/10/18/instalacao-geckodriver-driver-para-abrir-o-firefox-no-selenium/
+    	//Teste com firefox
+    	//System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+    	//Teste com chrome
+    	System.setProperty("webdriver.gecko.driver", "C:\\chromedriver.exe");
+        // abre firefox
+        WebDriver driver = new FirefoxDriver();
+
+        // acessa o site do google
+        driver.get("http://www.google.com.br/");
+
+        // digita no campo com nome "q" do google
+        WebElement campoDeTexto = driver.findElement(By.name("q"));
+        campoDeTexto.sendKeys("Caelum");
+
+        // submete o form
+        campoDeTexto.submit();
+
+    }
+}
