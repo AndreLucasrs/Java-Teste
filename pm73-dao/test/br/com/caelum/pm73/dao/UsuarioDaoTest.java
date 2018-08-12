7fe59a26b92089298852b3a58874455bdcd95b39
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-11 23:59:43 -0300

    Curso Teste de Integração: Testes SQL e DAOs automatizados em java, finalizado

--- pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -14,30 +14,30 @@ import br.com.caelum.pm73.dominio.Usuario;
 //OBS: como esse caso esta sendo feito com banco de dados em memoria estamos enserindo no momento do teste um novo usuario
 //o correto seria apenas trazer alguem ja esperado
 public class UsuarioDaoTest {
-	
+
 	private Session session;
 	private UsuarioDao usuarioDao;
 
 	@Before
 	public void antes() {
-		
+
 		session = new CriadorDeSessao().getSession();
 		usuarioDao = new UsuarioDao(session);
-		
-		//Com isso conseguimos resolver o problema citado no OBS
-		//podemos criar o dados
+
+		// Com isso conseguimos resolver o problema citado no OBS
+		// podemos criar o dados
 		session.beginTransaction();
 	}
 
 	@After
 	public void depois() {
-		
+
 		// e no final do teste ele vai da rollback no dado criado apenas para teste
 		// com isso os dados que estão no banco não ficaram "sujos" com dados de teste
 		session.getTransaction().rollback();
 		session.close();
 	}
-	
+
 	@Test
 	public void deveEncontrarPeloNomeEEmailMockado() {
 
@@ -47,7 +47,7 @@ public class UsuarioDaoTest {
 		// Não devemos mockar devido querer testar a consulta e o resultado que ela traz
 		Usuario usuario = usuarioDao.porNomeEEmail("Andre", "andre@teste.com.br");
 
-		//garantindo que o objeto é o esperado
+		// garantindo que o objeto é o esperado
 		assertEquals("Andre", usuario.getNome());
 		assertEquals("andre@teste.com.br", usuario.getEmail());
 	}
@@ -57,7 +57,7 @@ public class UsuarioDaoTest {
 
 		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("Lucas", "lucas@teste.com.br");
 
-		//garantido que volta null
+		// garantido que volta null
 		assertNull(usuarioDoBanco);
 	}
 }
